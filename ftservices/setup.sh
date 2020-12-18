8a2ec623e4189b6558b001a115fca0ee60a6dbf6
belke <bel-kebir.bouazza@smile.fr>
2020-12-18 15:09:01 +0100

    Initial commit

---
+++ ftservices/setup.sh
@@ -0,0 +1,18 @@
+minikube start --driver=docker
+
+#INSTALL METALLB
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+#INSTALL NGINX
+docker build -t s-nginx -f /nginx .
+kubectl apply -f config.yaml
+kubectl apply -f nginx.yaml
+
+#RUNNING DASHBOARD
+minikube dashboard &
